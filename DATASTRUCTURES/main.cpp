e5f5989c1a4b68737059982ccc4ba1e04d2c172a
AJAY N <najay19715@gmail.com>
2023-07-31 13:52:28 +0530

    datastructures

---
+++ DATASTRUCTURES/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int choice;
+	swith(true)
+	{
+		cout<<"1) Element inside at back";
+		cout<<"2) Element delete at back";
+		cout<<"3) Element insert at begin";
+		cout<<"4) Element delete at begin";
+		cout<<"5) Display the Array";
+		
+		cin>>choice;
+	}
+	
+}
